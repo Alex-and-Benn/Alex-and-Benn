f579f9d3cfff4b2b6308eeb27b1a2186c0b9a5cc
Benn Meyers <mb@norse-corp.com>
2014-05-18 15:11:08 -0700

    form php updated

--- form.php
+++ form.php
@@ -2,27 +2,26 @@
   <div id="homepage" class="container">
     <?php include("assets/includes/content/header.php"); ?>
       <?php
-      $con=mysqli_connect('localhost', 'root', 'root', 'database');
-      
-      // Check connection
-      if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-      } else {
-        echo "connected";
-      }
-      $sql="INSERT INTO `table` (field1) VALUES ('$_POST[field1]')";
-      if (!mysqli_query($con,$sql)) {
-        die('Error: ' . mysqli_error($con));
-      } else {
-        echo "1 record added";
-      }
-      mysqli_close($con);
+        if(isset($_POST['submit'])) {
+          $con=mysqli_connect('localhost', 'root', 'root', 'database');
+          // Check connection
+          if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+          }
+          $sql="INSERT INTO `table` (field1) VALUES ('$_POST[field1]')";
+          if (!mysqli_query($con,$sql)) {
+            die('Error: ' . mysqli_error($con));
+          } else {
+            echo "1 record added";
+          }
+          mysqli_close($con);
+        }
       ?>
       <div id="content">
         <div id="main_content">
           <form action="form.php" method="POST">
             <input type="text" name="field1" id="field1" value="">
-            <input type="submit" value="submit">
+            <input type="submit" name="submit" id="submit" value="submit">
           </form>
         </div><!-- #main_content -->
         <?php include("assets/includes/content/subNav.php"); ?>
