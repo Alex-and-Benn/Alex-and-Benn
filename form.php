d80a31c03aaae1acb334e28f34df7578b82f4d42
Benn Meyers <mb@norse-corp.com>
2014-05-18 14:43:53 -0700

    test database and form

---
+++ form.php
@@ -0,0 +1,32 @@
+<?php include("assets/includes/global/header.php"); ?>
+  <div id="homepage" class="container">
+    <?php include("assets/includes/content/header.php"); ?>
+      <?php
+      $con=mysqli_connect('localhost', 'root', 'root', 'reseller');
+      
+      // Check connection
+      if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      } else {
+        echo "connected";
+      }
+      $sql="INSERT INTO `reseller-contracts` (field1) VALUES ('$_POST[field1]')";
+      if (!mysqli_query($con,$sql)) {
+        die('Error: ' . mysqli_error($con));
+      } else {
+        echo "1 record added";
+      }
+      mysqli_close($con);
+      ?>
+      <div id="content">
+        <div id="main_content">
+          <form action="form.php" method="POST">
+            <input type="text" name="field1" id="field1" value="">
+            <input type="submit" value="submit">
+          </form>
+        </div><!-- #main_content -->
+        <?php include("assets/includes/content/subNav.php"); ?>
+      </div><!-- #content -->
+    <?php include("assets/includes/content/footer.php"); ?>
+  </div><!-- #homepage -->
+<?php include("assets/includes/global/footer.php"); ?>
--- test.php
+++ test.php
@@ -1,15 +1,17 @@
-<?php include("assets/includes/global/header.php"); ?>
-<div id="homepage">
-    <?php include("assets/includes/content/header.php"); ?>
+<?php
+$con=mysqli_connect('localhost', 'root', 'root', 'reseller');
 
-    <div id="content">
-    
-        <div id="main_content">
-            Hello World
-        </div><!-- #main_content -->
-        <?php include("assets/includes/content/subNav.php"); ?>
-
-    </div><!-- #content -->
-    <?php include("assets/includes/content/footer.php"); ?>
-</div><!-- #homepage -->
-<?php include("assets/includes/global/footer.php"); ?>
+// Check connection
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+	echo "connected";
+}
+$sql="INSERT INTO `reseller-contracts` (field1) VALUES ('$_POST[field1]')";
+if (!mysqli_query($con,$sql)) {
+  die('Error: ' . mysqli_error($con));
+} else {
+  echo "1 record added";
+}
+mysqli_close($con);
+?>
