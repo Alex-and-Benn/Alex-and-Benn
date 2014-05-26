4e0267553cd398064585c2728dc2ccc5a76c70d4
Benn Meyers <mb@norse-corp.com>
2014-05-26 12:36:17 -0700

    Include of jquery min

--- assets/includes/global-footer.php
+++
@@ -1,6 +0,0 @@
-<script src="//code.jquery.com/jquery.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/custom-shared.js"></script>
-<script src="assets/js/typeahead.bundle.min.js"></script>
-</body>
-</html>
---
+++ assets/js/norse.transitions.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $.fn.norse_transitions = function(a) {
+    /*$(this).children('.dynamic-caption').each(function(){
+      console.log(this.attr('data-transition'));
+    })
+    <dl>
+        <dt>slidein</dt>
+          <dd>A basic slide to the destination <small>(requires start coordinates)</small></dd>
+        <dt>spiral</dt>
+          <dd>Using a fibonacci spiral go to the destination <small>(requires start coordinates)</small></dd>
+        <dt>vibratehard</dt>
+          <dd>Using a hard spikey line go to the destination <small>(requires start coordinates)</small></dd>
+        <dt>vibratesoft</dt>
+          <dd>Using a soft curve line go to the destination <small>(requires start coordinates)</small></dd>
+        <dt>fade</dt>
+          <dd>starting from 0 opacity finish at 1</dd>
+        <dt>focus</dt>
+          <dd>go from blurry to clear</dd>
+        <dt>clap</dt>
+          <dd>like a rubber band snapping back to it's default form
+        <dt>zoomin</dt>
+          <dd>start from nothing and get larger</dd>
+        <dt>zoomout</dt>
+          <dd>shrink from the canvas size</dd>
+        <dt>form</dt>
+          <dd>form from particles (pixels)</dd>
+      </dl>
+			
+      case (a = 'slidein') {
+        
+      }*/
+  }
+})
