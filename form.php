6b30c455415218675b6ac5bfefcecb9e00339ec3
Benn Meyers <mb@norse-corp.com>
2014-06-09 00:05:02 -0700

    Added Table Retrieval script

--- form.php
+++ form.php
@@ -19,10 +19,33 @@
     ?>
       <div id="content">
         <div id="main_content">
-          <form action="form.php" method="POST">
-            <input type="text" name="field1" id="field1" value="" placeholder="Type an Item (e.g. Item1)">
-            <input type="submit" name="submit" id="submit" value="submit">
-          </form>
+          <div>
+          <h2>Content of table</h2>
+          <?php
+            $con=mysqli_connect("localhost", "root", "root", "database");
+            // Check connection
+            if (mysqli_connect_errno()) {
+              echo "Failed to connect to MySQL: " . mysqli_connect_error();
+              exit();
+            }
+            $result = mysqli_query($con,"SELECT * FROM `table`");
+            if(mysqli_fetch_array($result)) {
+              while($row = mysqli_fetch_array($result)) {
+                echo "<div>" . $row['field1'] . "</div>";
+              }
+            } else {
+              echo "<i>No table content</i>";
+            }
+            mysqli_close($con);
+          ?>
+          </div>
+          <div>
+            <h2>Insert into table</h2>
+            <form action="form.php" method="POST">
+              <input type="text" name="field1" id="field1" value="" placeholder="Type an Item (e.g. Item1)">
+              <input type="submit" name="submit" id="submit" value="submit">
+            </form>
+          </div>
         </div><!-- #main_content -->
         <?php include("assets/includes/sub-nav.php"); ?>
       </div><!-- #content -->
