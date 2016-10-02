a945641f77f7f272b0ea42166e0c9b7945f12219
Benjamin Meyers <benjamin@flowerjoiner.com>
2016-10-02 14:32:52 -0700

    Expanded Structure

--- assets/includes/fragments/main-nav.php
+++ assets/includes/fragments/main-nav.php
@@ -1,8 +1,8 @@
 <ul class="nav navbar-nav">
   <li><a href="index.php">home</a></li>
-  <li><a href="shop.php">shop</a></li>
+  <li><a href="#">shop</a></li>
   <li><a href="product.php">product</a></li>
   <li><a href="about.php">about</a></li>
   <li><a href="contact.php">contact</a></li>
-  <li><a href="blog.php">blog</a></li>
+  <li><a href="#">blog</a></li>
 </ul>
