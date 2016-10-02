13e3e8662c7a0349ddc47e093b6ca6d670345e25
Benjamin Meyers <benjamin@flowerjoiner.com>
2016-10-02 14:09:16 -0700

    Created Top Nav Fragment Include

---
+++ assets/includes/fragments/main-nav.php
@@ -0,0 +1,8 @@
+<ul class="nav navbar-nav">
+  <li><a href="index.php">home</a></li>
+  <li><a href="shop.php">shop</a></li>
+  <li><a href="product.php">product</a></li>
+  <li><a href="about.php">about</a></li>
+  <li><a href="contact.php">contact</a></li>
+  <li><a href="blog.php">blog</a></li>
+</ul>
--- assets/includes/main-header.php
+++ assets/includes/main-header.php
@@ -2,11 +2,7 @@
   <div class="container">
     <a class="navbar-brand" href="index.php">brand</a>
     <nav class="navbar-header">
-      <ul class="nav navbar-nav">
-        <li><a href="index.php">home</a></li>
-        <li><a href="form.php">form</a></li>
-        <li><a href="gallery.php">gallery <small class="status"><b>Alpha</b></small></a></li>
-      </ul>
+      <?php include("assets/includes/fragments/main-nav.php"); ?>
     </nav>
   </div>
 </header>
