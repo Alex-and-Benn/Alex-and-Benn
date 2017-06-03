8de455e4ea93d9bd64cf7047219db648a349b29a
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-06-02 21:44:30 -0700

    commenting code

--- product.php
+++ product.php
@@ -1,13 +1,17 @@
-<?php include("assets/includes/global-header.php"); ?>
-<?php include("assets/includes/main-header.php"); ?>
-<div id="homepage" class="container">
+<?php
+/*
+Including partial files most Content Management Systems (CMSs) divide out
+*/
+include("assets/includes/global-header.php"); // HTML head ?>
+<?php include("assets/includes/main-header.php"); // Visible header ?>
+<div id="product" class="container">
   <div id="content">
     <div id="main_content">
       <h1>product</h1>
       Hello World <a href="#">test</a>
     </div><!-- #main_content -->
-    <?php include("assets/includes/sub-nav.php"); ?>
+    <?php include("assets/includes/sub-nav.php"); // An optional sub navigation ?>
   </div><!-- #content -->
-  <?php include("assets/includes/main-footer.php"); ?>
-</div><!-- #homepage -->
-<?php include("assets/includes/global-footer.php"); ?>
+  <?php include("assets/includes/main-footer.php"); // Visible footer ?>
+</div><!-- #product -->
+<?php include("assets/includes/global-footer.php"); // HTML foot ?>
