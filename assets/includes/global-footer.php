9b186527a9b64bfdd370461ed1d665b1ab369bf3
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-06-02 22:14:21 -0700

    removed typeahead plugin

--- assets/includes/global-footer.php
+++ assets/includes/global-footer.php
@@ -5,7 +5,6 @@ BEST PRACTICE: Include Javascripts after the content has been loaded.
 ?>
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/typeahead.bundle.min.js"></script>
 <script src="assets/js/custom-shared.js"></script>
 </body>
 </html>
