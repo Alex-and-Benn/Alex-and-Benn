07fa422a43c1781317befe2920bd1bd4c1ef86bf
Benjamin Meyers <benjamin@purespiritwebworks.com>
2020-04-14 18:09:33 -0700

    adding fontawesome stylesheet

--- assets/includes/global-header.php
+++ assets/includes/global-header.php
@@ -15,6 +15,7 @@
     */
     ?>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" rel="stylesheet" type="text/css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="assets/css/custom.css" rel="stylesheet" type="text/css" media="screen">
 </head>
 <body>
